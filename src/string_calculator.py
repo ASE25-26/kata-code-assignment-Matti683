ca8115b90591725a131a23091176f4e680ee5d5e
BuildTools <unconfigured@null.spigotmc.org>
2025-11-06 16:47:39 +0100

    Kata gelöst

--- src/string_calculator.py
+++ src/string_calculator.py
@@ -1,5 +1,35 @@
 import re
 
+from PIL.ImagePalette import sepia
+from scipy.special import special
+
+
 def add(numbers: str) -> int:
-    # Implementiere die String-Kalkulator-Funktion hier
-    return sum()
+    if numbers == "":
+        return 0
+    if numbers.isdigit():
+        return int(numbers)
+
+    check_for_separators = re.split("[,\n]+", numbers)
+    separators = find_separators(check_for_separators)
+
+    strings_of_numbers = re.split("[" + separators + "]+", numbers.replace("//", "").replace("[", "").replace("]", ""))
+    numbers = list(filter(lambda z: z<=1000, map(lambda x: int(x), filter(lambda y: not y.startswith("//") and y not in separators, strings_of_numbers))))
+
+    negative_numbers = list(filter(lambda n: n<0, numbers))
+    if len(negative_numbers)>0:
+        message = "String contains negative numbers: " + str(negative_numbers[0])
+        for n in negative_numbers:
+            if n != negative_numbers[0]:
+                message = message + ", " + str(n)
+        raise ValueError(message)
+
+    return sum(numbers)
+
+
+def find_separators(check_for_separators: list[str]) -> str:
+    separators = ",\n"
+    for s in check_for_separators:
+        if s.startswith("//"):
+            separators = separators + s.replace("//", "").replace("[", "").replace("]", "")
+    return separators
